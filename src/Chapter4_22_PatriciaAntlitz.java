e64fa98bd3ac733668b2f21caddc428d8bb61b57
patybn3 <paty.nigro@gmail.com>
2022-10-09 15:46:59 -0400

    added required methods

--- src/Chapter4_22_PatriciaAntlitz.java
+++ src/Chapter4_22_PatriciaAntlitz.java
@@ -2,6 +2,36 @@ public class Chapter4_22_PatriciaAntlitz
 {
     public static void main(String[] args)
     {
+        LinkedQueue<Integer> newList = new LinkedQueue<>();
 
+        //this loop will insert 10 numbers into the queue
+        System.out.println("Numbers on This List:");
+        for(int i = 0; i < 10; i++)
+        {
+            //enqueue method will received numbers from 0 - 9
+            newList.enqueue(i);
+        }
+        //prints the list
+        newList.toString();
+        //size of the list
+        System.out.println("Size: " + newList.size());
+        //spaces available
+        System.out.println("Empty Space: " + newList.space());
+        //will remove two items from the front
+        newList.remove(2);
+        newList.toString();
+
+        newList.swapStart();
+        newList.toString();
+
+        newList.swapEnd();
+        newList.toString();
+        //tests the dequeue method
+        System.out.println("\nDequeue Example: ");
+        newList.dequeue();
+        newList.toString();
+        //prints the empty space again to test
+        System.out.println();
+        System.out.println("Empty Space: " + newList.space());
     }
 }
